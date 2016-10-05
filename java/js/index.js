133e84e2e8851dccd69913b3d33ae1abe9dac03e
DylanLam2016 <20lamjd2@webmail.sis.edu.hk>
2016-10-05 09:59:46 +0800

    A random quiz.

---
+++ java/js/index.js
@@ -0,0 +1,42 @@
+function quiz() {
+  var question1 = prompt("What animal is faster, a Cheetah or a Eagle?");
+
+  if (question1 === "Cheetah") {
+    alert("Correct");
+  } else {
+    alert("Wrong");
+  }
+
+  var question2 = prompt("What is the biggest skyscraper in the world?");
+
+  if (question2 === "Burj Khalifa") {
+    alert("Correct");
+  } else {
+    alert("Wrong");
+  }
+
+  var question3 = prompt("Which WWE wrestler was famous because of his intro theme song?");
+
+  if (question3 === "John Cena") {
+    alert("Correct");
+  } else {
+    alert("Wrong");
+  }
+
+  var question4 = prompt("In the WWE, which is the most famous special move?");
+
+  if (question4 === "RKO") {
+    alert("Correct");
+  } else {
+    alert("Wrong");
+  }
+
+  var question5 = prompt("What is the most expesive transfer bid on a player in football aka.Soccer in the world? Full name please.");
+
+  if (question5 === "Paul Pogba") {
+    alert("Correct");
+  } else {
+    alert("Wrong");
+  }
+
+}
